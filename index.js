00d69fdc3727bd44c072b3705a3ccb4804512f89
Ryan Schultz <ryan_schultz@graycor.com>
2018-06-21 12:29:01 -0500

    Bypass the locale stuff if Expo is undefined

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import I18n from './vendor/i18n';
-import Exponent from 'expo';
+import Expo from 'expo';
 
 I18n.initAsync = async () => {
-  const Localization = Expo.DangerZone.Localization || Expo.Localization || Expo.Util;
-  const locale = await Localization.getCurrentLocaleAsync();
-  I18n.locale = (locale) ? locale.replace(/_/, '-') : '';
+  if (Expo !== undefined) {
+    const Localization = Expo.DangerZone.Localization || Expo.Localization || Expo.Util;
+    const locale = await Localization.getCurrentLocaleAsync();
+    I18n.locale = (locale) ? locale.replace(/_/, '-') : '';
+  }
 }
 
 export default I18n;
